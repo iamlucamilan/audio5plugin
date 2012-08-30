1414568a7fced9ad376fdeaf7a8b497561b833ed
Luca Vibes Milan <milan.luca@gmail.com>
2012-08-30 23:01:28 +0200

    added the empty development version

---
+++ audio5_dev.jQuery.js
@@ -0,0 +1,39 @@
+/**
+ * Audio5.js
+ *
+ * Audio5.js is a little script that "convert" flash player in tumblr audio post to html5 audio tag
+ *
+ *
+ * Copyright 2012 Luca Milan (@LucaVibesMilan)
+ *
+ * Released under the MIT and GPL Licenses.
+ *
+ * ------------------------------------------------
+ *  author:  Luca Milan
+ *  version: 0.0.1
+ *	url: 	 http://audiopostshtml5.tumblr.com/
+ *  source:  https://github.com/LucaVibesMilan/audio5.js
+ */
+
+
+(function($){
+
+$.fn.center = function(){
+
+var element = this;
+
+$(element).load(function(){
+
+audio5();
+
+function audio5(){
+
+    
+
+    });
+};
+});
+
+};
+
+})(jQuery);
